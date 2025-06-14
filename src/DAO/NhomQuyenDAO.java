57dae20a930c1ac3afca325d49a4edd4d255241d
tuepham634 <152422581+tuepham634@users.noreply.github.com>
2025-06-14 22:19:50 +0700

    Tuệ gộp TaiKhoanDAO +NhomQuyenDAO với Việt

--- src/DAO/NhomQuyenDAO.java
+++ src/DAO/NhomQuyenDAO.java
@@ -1,15 +1,14 @@
 package DAO;
 
 import DTO.NhomQuyenDTO;
-import config.ketnoiDTB;
+import config.JDBC;
 
 import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.util.ArrayList;
-import java.util.logging.Level;
-import java.util.logging.Logger;
+
 
 /**
  *
@@ -17,39 +16,34 @@ import java.util.logging.Logger;
  */
 public class NhomQuyenDAO implements DAOinterface<NhomQuyenDTO> {
 
-    public static NhomQuyenDAO getInstance() {
+    public static  NhomQuyenDAO getInstance(){
         return new NhomQuyenDAO();
     }
-
     @Override
-    public int insert(NhomQuyenDTO t) {
-        int result = 0;
-        try {
-            Connection con = (Connection) ketnoiDTB.getConnection();
-            String sql = "INSERT INTO `nhomquyen`(`tennhomquyen`,`trangthai`) VALUES (?,1)";
-            PreparedStatement pst = con.prepareStatement(sql);
-            pst.setString(1, t.getTennhomquyen());
+    public int insert(NhomQuyenDTO nhomQuyen) {
+        int result =0;
+        String sql = "INSERT INTO nhomquyen(tennhomquyen,trangthai) VALUES (?,1)";
+        try(Connection conn = JDBC.getConnection();
+            PreparedStatement pst = conn.prepareStatement(sql)){
+            pst.setString(1,nhomQuyen.getTennhomquyen());
             result = pst.executeUpdate();
-            ketnoiDTB.closeConnection(con);
-        } catch (SQLException ex) {
-            Logger.getLogger(NhomQuyenDAO.class.getName()).log(Level.SEVERE, null, ex);
+        }catch (SQLException e){
+            e.printStackTrace();
         }
         return result;
     }
 
     @Override
-    public int update(NhomQuyenDTO t) {
+    public int update(NhomQuyenDTO nhomQuyen) {
         int result = 0;
-        try {
-            Connection con = (Connection) ketnoiDTB.getConnection();
-            String sql = "UPDATE `nhomquyen` SET `tennhomquyen`=? WHERE `manhomquyen`=?";
-            PreparedStatement pst = con.prepareStatement(sql);
-            pst.setString(1, t.getTennhomquyen());
-            pst.setInt(2, t.getManhomquyen());
+        String sql = "UPDATE nhomquyen SET tennhomquyen=? WHERE manhomquyen=?";
+        try(Connection conn = JDBC.getConnection();//mở kết nối đến csdl
+            PreparedStatement pst = conn.prepareStatement(sql)){// khảo tạo độis tươn từ cái sql  để có thể gán giá trị truyeeng vòa ?,? :1,2
+            pst.setString(1,nhomQuyen.getTennhomquyen());
+            pst.setInt(2,nhomQuyen.getManhomquyen());
             result = pst.executeUpdate();
-            ketnoiDTB.closeConnection(con);
-        } catch (SQLException ex) {
-            Logger.getLogger(NhomQuyenDAO.class.getName()).log(Level.SEVERE, null, ex);
+        }catch (SQLException e){
+            e.printStackTrace();
         }
         return result;
     }
@@ -57,67 +51,65 @@ public class NhomQuyenDAO implements DAOinterface<NhomQuyenDTO> {
     @Override
     public int delete(String t) {
         int result = 0;
-        try {
-            Connection con = (Connection) ketnoiDTB.getConnection();
-            String sql = "UPDATE `nhomquyen` SET `trangthai` = 0 WHERE manhomquyen = ?";
-            PreparedStatement pst = con.prepareStatement(sql);
-            pst.setString(1, t);
-            result = pst.executeUpdate();
-            ketnoiDTB.closeConnection(con);
-        } catch (SQLException ex) {
-            Logger.getLogger(NhomQuyenDAO.class.getName()).log(Level.SEVERE, null, ex);
+        String sql = "UPDATE nhomquyen SET trangthai = 0 WHERE manhomquyen = ? ";
+        try(Connection conn = JDBC.getConnection();
+            PreparedStatement pst = conn.prepareStatement(sql)){
+            pst.setString(1,t);
+            result =pst.executeUpdate();
+        }catch (SQLException e){
+            e.printStackTrace();
         }
         return result;
     }
+
 
     @Override
     public ArrayList<NhomQuyenDTO> selectAll() {
-        ArrayList<NhomQuyenDTO> result = new ArrayList<NhomQuyenDTO>();
-        try {
-            Connection con = (Connection) ketnoiDTB.getConnection();
-            String sql = "SELECT * FROM nhomquyen WHERE trangthai = 1";
-            PreparedStatement pst = (PreparedStatement) con.prepareStatement(sql);
-            ResultSet rs = (ResultSet) pst.executeQuery();
-            while (rs.next()) {
+        ArrayList<NhomQuyenDTO> list = new ArrayList<>();
+        String sql = "SELECT * FROM nhomquyen WHERE trangthai = 1";
+        try(Connection conn = JDBC.getConnection();
+            PreparedStatement pst = conn.prepareStatement(sql)){
+            ResultSet rs = pst.executeQuery();
+            while (rs.next()){
                 int manhomquyen = rs.getInt("manhomquyen");
                 String tennhomquyen = rs.getString("tennhomquyen");
-                NhomQuyenDTO dvt = new NhomQuyenDTO(manhomquyen, tennhomquyen);
-                result.add(dvt);
+                NhomQuyenDTO dvt = new NhomQuyenDTO(manhomquyen,tennhomquyen);
+                list.add(dvt);
             }
-            ketnoiDTB.closeConnection(con);
-        } catch (Exception e) {
+
+        } catch (SQLException e){
+            e.printStackTrace();
         }
-        return result;
+        return list;
     }
 
     @Override
-    public NhomQuyenDTO selectById(String t) {
-        NhomQuyenDTO result = null;
-        try {
-            Connection con = (Connection) ketnoiDTB.getConnection();
-            String sql = "SELECT * FROM nhomquyen WHERE manhomquyen=?";
-            PreparedStatement pst = (PreparedStatement) con.prepareStatement(sql);
-            pst.setString(1, t);
-            ResultSet rs = (ResultSet) pst.executeQuery();
-            while (rs.next()) {
+    public NhomQuyenDTO selectById(int t) {
+        NhomQuyenDTO list = null;
+        String sql = "SELECT * FROM nhomquyen WHERE manhomquyen = ?";
+        try(Connection conn = JDBC.getConnection();
+            PreparedStatement pst = conn.prepareStatement(sql)){
+            pst.setInt(1,t);
+            ResultSet rs = pst.executeQuery();
+            while (rs.next()){
                 int manhomquyen = rs.getInt("manhomquyen");
                 String tennhomquyen = rs.getString("tennhomquyen");
-                result = new NhomQuyenDTO(manhomquyen, tennhomquyen);
+                list = new NhomQuyenDTO(manhomquyen,tennhomquyen);
             }
-            ketnoiDTB.closeConnection(con);
-        } catch (Exception e) {
+        } catch (SQLException e) {
+
         }
-        return result;
+        return list;
     }
 
     @Override
     public int getAutoIncrement() {
         int result = -1;
         try {
-            Connection con = (Connection) ketnoiDTB.getConnection();
+            Connection con = JDBC.getConnection();
             String sql = "SELECT `AUTO_INCREMENT` FROM  INFORMATION_SCHEMA.TABLES WHERE TABLE_SCHEMA = 'quanlikhohang' AND   TABLE_NAME   = 'nhomquyen'";
-            PreparedStatement pst = (PreparedStatement) con.prepareStatement(sql);
-            ResultSet rs2 = pst.executeQuery(sql);
+            PreparedStatement pst =  con.prepareStatement(sql);
+            ResultSet rs2 = pst.executeQuery();
             if (!rs2.isBeforeFirst()) {
                 System.out.println("No data");
             } else {
@@ -126,8 +118,8 @@ public class NhomQuyenDAO implements DAOinterface<NhomQuyenDTO> {
 
                 }
             }
-        } catch (SQLException ex) {
-            Logger.getLogger(NhomQuyenDAO.class.getName()).log(Level.SEVERE, null, ex);
+        } catch (SQLException e) {
+            e.printStackTrace();
         }
         return result;
     }
--- src/DTO/NhomQuyenDTO.java
+++ src/DTO/NhomQuyenDTO.java
@@ -4,21 +4,16 @@
  */
 package DTO;
 
-import java.util.Objects;
-
 
 public class NhomQuyenDTO {
     private int manhomquyen;
     private String tennhomquyen;
-
     public NhomQuyenDTO() {
     }
-
     public NhomQuyenDTO(int manhomquyen, String tennhomquyen) {
         this.manhomquyen = manhomquyen;
         this.tennhomquyen = tennhomquyen;
     }
-
     public int getManhomquyen() {
         return manhomquyen;
     }
@@ -34,36 +29,4 @@ public class NhomQuyenDTO {
     public void setTennhomquyen(String tennhomquyen) {
         this.tennhomquyen = tennhomquyen;
     }
-
-    @Override
-    public int hashCode() {
-        int hash = 3;
-        hash = 37 * hash + this.manhomquyen;
-        hash = 37 * hash + Objects.hashCode(this.tennhomquyen);
-        return hash;
-    }
-
-    @Override
-    public boolean equals(Object obj) {
-        if (this == obj) {
-            return true;
-        }
-        if (obj == null) {
-            return false;
-        }
-        if (getClass() != obj.getClass()) {
-            return false;
-        }
-        final NhomQuyenDTO other = (NhomQuyenDTO) obj;
-        if (this.manhomquyen != other.manhomquyen) {
-            return false;
-        }
-        return Objects.equals(this.tennhomquyen, other.tennhomquyen);
-    }
-
-    @Override
-    public String toString() {
-        return "NhomQuyenDTO{" + "manhomquyen=" + manhomquyen + ", tennhomquyen=" + tennhomquyen + '}';
-    }
-    
 }
